ed10b235683b8d972c0664356f1bdb004517306e
junki <wnsrlsladlek@naver.com>
2022-10-03 16:58:09 +0900

    feat(main): lambda 배포에 맞게 수정

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
 import json
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-
-def temp_func():
+def my_handler(event, context):
+    print("Hello, lambda")
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from ECR Test!')
+        'body': json.dumps('Hello from Lambda!')
     }
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('test')
-    temp_func()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
